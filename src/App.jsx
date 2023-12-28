64793a3f79cee880dd93cdc7ee221ab6a1bdeb23
Sarvesh <sarveshjaiofficial@gmail.com>
2023-12-28 21:38:23 +0530

    finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,44 @@
-import './App.css';
+import "./App.css";
 import elephant from "./images/elephant.jpeg";
 
-const imageData = ()=>{
+const imageData = () => {
   let data = [
     {
-      id:1,
-      img:elephant
+      id: 1,
+      img: elephant,
     },
     {
-      id:2,
-      img:elephant
+      id: 2,
+      img: elephant,
     },
     {
-      id:3,
-      img:elephant
+      id: 3,
+      img: elephant,
     },
     {
-      id:4,
-      img:elephant
-    }
-  ]
+      id: 4,
+      img: elephant,
+    },
+  ];
   return data;
-}
+};
 
 function App() {
   // code here
+  let data = imageData();
+  return (
+    <>
+      <h1> Kalvium Gallery</h1>
+      <div className="gallery">
+        {data.map((elem) => {
+          return <img src={elem.img} className="image" key={elem.id}></img>;
+        })}
+        <small>
+          <p>With ❤️ by Sarvesh</p>
+        </small>
+      </div>
+    </>
+  );
 }
 
 export default App;
--- src/AppClass.jsx
+++ src/AppClass.jsx
@@ -1,30 +1,29 @@
 import { Component } from "react";
-import "./App.css"
+import "./App.css";
 import elephant from "./images/elephant.jpeg";
 
-export default class AppClass extends Component{
-  
-  imageData = ()=>{
+export default class AppClass extends Component {
+  imageData = () => {
     let data = [
       {
-        id:1,
-        img:elephant
+        id: 1,
+        img: elephant,
       },
       {
-        id:2,
-        img:elephant
+        id: 2,
+        img: elephant,
       },
       {
-        id:3,
-        img:elephant
+        id: 3,
+        img: elephant,
       },
       {
-        id:4,
-        img:elephant
-      }
-    ]
+        id: 4,
+        img: elephant,
+      },
+    ];
     return data;
-  }
+  };
   // code here
   render() {
     let image_arr = this.imageData();
@@ -33,11 +32,13 @@ export default class AppClass extends Component{
         <h1> Kalvium Gallery</h1>
         <div className="gallery">
           {image_arr.map((elem) => {
-            return <img src={elem.img} className="image"></img>;
+            return <img src={elem.img} className="image" key={elem.id}></img>;
           })}
-          <small><p>With ❤️ by Sarvesh</p></small>
+          <small>
+            <p>With ❤️ by Sarvesh</p>
+          </small>
         </div>
       </>
     );
   }
-  }
+}
