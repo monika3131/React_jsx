270139c1e49b0bfa02dcad9cb862ed84400639a1
bhagirathauti <bhagirath961778@gmail.com>
2024-01-06 15:19:55 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -22,9 +22,37 @@ const imageData = ()=>{
   ]
   return data;
 }
-
+let data = imageData()
 function App() {
   // code here
+  let galleryStyle = {
+    textAlign:"center"
+  }
+  let divStyle = {
+    padding:"50px",
+    display:"grid",
+    gridTemplateColumns:"repeat(2,1fr)",
+    placeItems : "Center",
+    gap:"50px"
+  }
+  return(
+    <>
+    <h1 style={galleryStyle}>Kalvium Gallery</h1>
+    {/* <div style={divStyle}>
+      <img src={data[0].img} width={"500px"} height={"350px"} />
+      <img src={data[1].img} width={"500px"} height={"350px"} />
+      <img src={data[2].img} width={"500px"} height={"350px"} />
+      <img src={data[3].img} width={"500px"} height={"350px"} />
+    </div> */}
+    <div style={divStyle}>
+      {data.map((el)=>(
+        <div key={el.id}>
+          <img src={el.img} width={"680px"} height={"400px"}/>
+        </div>
+      ))}
+    </div>
+    </>
+  )
 }
 
 export default App;
